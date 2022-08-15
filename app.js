19012e65628fd4e41b41fdc78912867f7eae6d69
fahimahammed <fahimahammed.cse@gmail.com>
2022-08-16 01:58:04 +0600

    js clock

---
+++ app.js
@@ -0,0 +1,22 @@
+const secLine = document.getElementById('second-line');
+const minLine = document.getElementById('minute-line');
+const hourLine = document.getElementById('hour-line');
+
+const setDate = () =>{
+    const currentDate = new Date();
+    
+    const seconds = currentDate.getSeconds();
+    const secondDeg = ((seconds/60)*360) + 90;
+    secLine.style.transform = `rotate(${secondDeg}deg)`;
+
+    const minutes = currentDate.getMinutes();
+    const minuteDeg = ((minutes/60)*360) + ((seconds/60)*6) + 90;
+    minLine.style.transform = `rotate(${minuteDeg}deg)`;
+
+    const hours = currentDate.getHours();
+    const hourDeg = ((hours/60)*360) + ((minutes/60)*30)+ 90;
+    hourLine.style.transform = `rotate(${hourDeg}deg)`;
+}
+
+setInterval(setDate, 1000);
+setDate();
